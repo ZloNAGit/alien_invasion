dc3a55c2a7cdaacd02e23a8768555c83db281f95
Thomas Rose <thomascrose90@gmail.com>
2022-01-19 16:53:14 -0500

    Refactor _create_fleet out into a _create_alien helper function.

--- alien_invasion.py
+++ alien_invasion.py
@@ -93,7 +93,6 @@ class AlienInvasion:
     def _create_fleet(self):
         """Create the fleet of aliens."""
 
-        # Create an alien and find the number of aliens in a row.
         # Spacing between each alien is equal to one alien width.
         alien = Alien(self)
         alien_width = alien.rect.width
@@ -102,11 +101,16 @@ class AlienInvasion:
 
         # Create the first row of aliens.
         for alien_number in range (number_aliens_x):
-            # Create an alien and place it in the row.
-            alien = Alien(self)
-            alien.x = alien_width + 2 * alien_width * alien_number
-            alien.rect.x = alien.x
-            self.aliens.add(alien)
+            self._create_alien(alien_number)
+
+    def _create_alien(self, alien_number):
+        """Create an alien and place it in the row."""
+        # Create an alien and place it in the row.
+        alien = Alien(self)
+        alien_width = alien.rect.width
+        alien.x = alien_width + 2 * alien_width * alien_number
+        alien.rect.x = alien.x
+        self.aliens.add(alien)
 
 if __name__ == '__main__':
     # Make a game instance, and run the game.
