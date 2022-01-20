deb4108f45c2e71b687747f86e7573124d994820
Thomas Rose <thomascrose90@gmail.com>
2022-01-20 16:13:40 -0500

    Add ship image to indicate remaining player ships in top left corner.

--- alien_invasion.py
+++ alien_invasion.py
@@ -206,6 +206,7 @@ class AlienInvasion:
         if self.stats.ships_left > 0:
             # Decrement ships_left
             self.stats.ships_left -= 1
+            self.sb.prep_ships()
 
             # Get rid of any remaining aliens and bullets.
             self.aliens.empty()
@@ -240,6 +241,7 @@ class AlienInvasion:
             self.stats.game_active = True
             self.sb.prep_score()
             self.sb.prep_level()
+            self.sb.prep_ships()
 
             # Get rid of any remaining aliens and bullets.
             self.aliens.empty()
