038ae610ee9d15b77d2eb58ed895927d3ba3d55d
Thomas Rose <thomascrose90@gmail.com>
2022-01-20 15:33:52 -0500

    Adding logic to ensure every alien is giving points in the event more than one alien is destroyed per loop.

--- alien_invasion.py
+++ alien_invasion.py
@@ -127,7 +127,8 @@ class AlienInvasion:
             self.bullets, self.aliens, True, True)
 
         if collisions:
-            self.stats.score += self.settings.alien_points
+            for aliens in collisions.values():
+                self.stats.score += self.settings.alien_points * len(aliens)
             self.sb.prep_score()
 
         if not self.aliens:
@@ -232,6 +233,7 @@ class AlienInvasion:
             self.settings.initialize_dynamic_settings()
             self.stats.reset_stats()
             self.stats.game_active = True
+            self.sb.prep_score()
 
             # Get rid of any remaining aliens and bullets.
             self.aliens.empty()
